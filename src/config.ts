572b340ce3e214b70dd2cf7dc3fa931a32c02ed4
smallGuDu <158005463+smallGuDu@users.noreply.github.com>
2025-11-02 10:24:15 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -50,11 +50,11 @@ export const siteConfig: SiteConfig = {
 	},
 
 	bangumi: {
-		userId: "your-bangumi-id", // 在此处设置你的Bangumi用户ID，可以设置为 "sai" 测试
+		userId: "1174493", // 在此处设置你的Bangumi用户ID，可以设置为 "sai" 测试
 	},
 
 	anime: {
-		mode: "local", // 番剧页面模式："bangumi" 使用Bangumi API，"local" 使用本地配置
+		mode: "bangumi", // 番剧页面模式："bangumi" 使用Bangumi API，"local" 使用本地配置
 	},
 
 	// 文章列表布局配置
