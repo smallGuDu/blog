dec96e75fdfb7fa9ff99d182131c576bcd34fb14
smallGuDu <158005463+smallGuDu@users.noreply.github.com>
2025-11-02 10:31:38 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,9 +27,7 @@ import { remarkMermaid } from "./src/plugins/remark-mermaid.js";
 import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
-	site: "https://smallgudu.github.io/",
-
-	base: "/blog",
+	site: "https://blog.soliar.top/",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
