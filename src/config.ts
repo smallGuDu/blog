15c718c03d857b6de4859ca0025712e253a3ff96
smallGuDu <158005463+smallGuDu@users.noreply.github.com>
2026-05-04 19:12:45 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -329,7 +329,7 @@ export const musicPlayerConfig: MusicPlayerConfig = {
 	mode: "meting", // 音乐播放器模式，可选 "local" 或 "meting"
 	meting_api:
 		"https://meting-api.civer.cn/api", // Meting API 地址
-	id: "13241357395", // 歌单ID
+	id: "17946372294", // 歌单ID
 	server: "netease", // 音乐源服务器。有的meting的api源支持更多平台,一般来说,netease=网易云音乐, tencent=QQ音乐, kugou=酷狗音乐, xiami=虾米音乐, baidu=百度音乐
 	type: "playlist", // 播单类型
 };
